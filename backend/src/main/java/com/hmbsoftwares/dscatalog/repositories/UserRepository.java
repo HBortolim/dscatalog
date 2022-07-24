b4bee35b671e0c9f90c56712587213143ca0f63d
Henrique Bortolim <hbortolim@gmail.com>
2022-07-24 17:25:26 -0300

    project update

--- backend/src/main/java/com/hmbsoftwares/dscatalog/repositories/UserRepository.java
+++ backend/src/main/java/com/hmbsoftwares/dscatalog/repositories/UserRepository.java
@@ -4,6 +4,5 @@ import com.hmbsoftwares.dscatalog.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
     User findByEmail(String email);
 }
