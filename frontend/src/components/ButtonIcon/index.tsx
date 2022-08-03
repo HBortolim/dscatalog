2baa88ad1493e333f21a4311c5c4817a57c97e5f
Henrique Bortolim <hbortolim@gmail.com>
2022-08-02 21:27:59 -0300

    Home page styling

---
+++ frontend/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+
+    return (
+        <button className="btn btn-primary btn-icon">
+            <p>Inicie agora a sua busca</p>
+        </button>
+    )
+}
+
+export default ButtonIcon;
