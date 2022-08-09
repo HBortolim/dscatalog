93e6c008de70dd94731df38c3fe3773b0cf8b9e6
Henrique Bortolim <hbortolim@gmail.com>
2022-08-09 19:18:14 -0300

    Catalog - ProductDetails link

--- frontend/src/pages/ProductDetails/indes.tsx
+++ frontend/src/pages/ProductDetails/indes.tsx
@@ -1,37 +1,50 @@
-import { ReactComponent as ArrowIcon} from 'assets/img/arrow.svg';
-import ProductPrice from 'components/ProductPrice';
-import './styles.css';
-
+import { ReactComponent as ArrowIcon } from "assets/img/arrow.svg";
+import ProductPrice from "components/ProductPrice";
+import { Link } from "react-router-dom";
+import "./styles.css";
 
 const ProductDetails = () => {
-
-    return(
-        <div className="product-details-container">
-            <div className="product-details-card base-card">
-                <div className="goback-container">
-                    <ArrowIcon/>
-                    <h2>Voltar</h2>
-                </div>
-                <div className="row">
-                    <div className="col-xl-6">
-                        <div className="img-container">
-                            <img src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg" alt="Nome do Produto" />
-                        </div>
-                        <div className="name-price-container">
-                            <h1>Nome do Produto</h1>
-                            <ProductPrice price={2345.67}/>
-                        </div>
-                    </div>
-                    <div className="col-xl-6">
-                        <div className="description-container">
-                            <h2>Descrição do Produto</h2>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum eaque recusandae mollitia cum! Eaque enim doloribus nulla labore vel laboriosam consequuntur, eveniet voluptatem alias optio, corporis magni ad! Laudantium, itaque! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa omnis nam cumque soluta sequi magnam vitae optio eaque consequuntur beatae, alias iusto labore, dolores minima tempore at natus nihil accusantium!</p>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="product-details-container">
+      <div className="product-details-card base-card">
+        <Link to="/products">
+          <div className="goback-container">
+            <ArrowIcon />
+            <h2>Voltar</h2>
+          </div>
+        </Link>
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="img-container">
+              <img
+                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg"
+                alt="Nome do Produto"
+              />
             </div>
+            <div className="name-price-container">
+              <h1>Nome do Produto</h1>
+              <ProductPrice price={2345.67} />
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="description-container">
+              <h2>Descrição do Produto</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Laborum eaque recusandae mollitia cum! Eaque enim doloribus
+                nulla labore vel laboriosam consequuntur, eveniet voluptatem
+                alias optio, corporis magni ad! Laudantium, itaque! Lorem ipsum
+                dolor sit amet, consectetur adipisicing elit. Culpa omnis nam
+                cumque soluta sequi magnam vitae optio eaque consequuntur
+                beatae, alias iusto labore, dolores minima tempore at natus
+                nihil accusantium!
+              </p>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default ProductDetails;
