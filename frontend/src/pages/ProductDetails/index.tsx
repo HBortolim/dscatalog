72a1f6f6c7b05b860e44c8a93d1544c08655bdbd
Henrique Bortolim <hbortolim@gmail.com>
2022-08-11 20:36:58 -0300

    useState, useEffect, useParams

--- frontend/src/pages/ProductDetails/index.tsx
+++ frontend/src/pages/ProductDetails/index.tsx
@@ -1,9 +1,29 @@
 import { ReactComponent as ArrowIcon } from "assets/img/arrow.svg";
+import axios from "axios";
 import ProductPrice from "components/ProductPrice";
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { Product } from "types/product";
+import { BASE_URL } from "util/requests";
 import "./styles.css";
 
+type UrlParams = {
+  productId: string;
+}
+
 const ProductDetails = () => {
+
+  const { productId } = useParams<UrlParams>();
+  // atributo e função para armazenar o estado
+  const [product, setProduct] = useState<Product>();
+  // na montagem do componente => funcao que quer executar e uma lista de dependencias(a lista de objetos que quer observar e qnd ele alterar, vai rodar a função de novo)
+  useEffect(() => {
+    axios.get(`${BASE_URL}/products/${productId}`)
+    .then(response => {
+      setProduct(response.data);
+    });
+  }, [productId]) ; 
+
   return (
     <div className="product-details-container">
       <div className="product-details-card base-card">
@@ -17,18 +37,19 @@ const ProductDetails = () => {
           <div className="col-xl-6">
             <div className="img-container">
               <img
-                src="https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg"
-                alt="Nome do Produto"
+                src={product?.imgUrl}
+                alt={product?.name}
               />
             </div>
             <div className="name-price-container">
-              <h1>Nome do Produto</h1>
-              <ProductPrice price={2345.67} />
+              <h1>{product?.name}</h1>
+              {/*vai renderizar se isso for verdade*/}
+              {product && <ProductPrice price={product?.price} />}
             </div>
           </div>
           <div className="col-xl-6">
             <div className="description-container">
-              <h2>Descrição do Produto</h2>
+              <h2>{product?.description}</h2>
               <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                 Laborum eaque recusandae mollitia cum! Eaque enim doloribus
