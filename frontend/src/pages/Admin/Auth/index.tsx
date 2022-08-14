ed3ed2326fb6bb007e9baebb2fc77e0099486aa2
Henrique Bortolim <hbortolim@gmail.com>
2022-08-14 17:53:01 -0300

    auth routes

---
+++ frontend/src/pages/Admin/Auth/index.tsx
@@ -0,0 +1,29 @@
+import { ReactComponent as AuthImage } from 'assets/img/auth-img.svg';
+import { Route, Switch } from 'react-router-dom';
+
+const Auth = () => {
+    return(
+        <div className="auth-container">
+            <div className="auth-banner-container">
+                <h1>Divulge seus produtos no DSCatalog</h1>
+                <p>Faça parte do nosso catálogo de divulgação e aumente a venda dos seus produtos.</p>
+                <AuthImage/>
+            </div>
+            <div className="auth-form-container">
+                <Switch>
+                    <Route path="/admin/auth/login">
+        	            <h1>Card de login</h1>
+                    </Route>
+                    <Route path="/admin/auth/signup">
+        	            <h1>Card de cadastro</h1>
+                    </Route>
+                    <Route path="/admin/auth/recover">
+        	            <h1>Card de senha</h1>
+                    </Route>
+                </Switch>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -8,13 +8,13 @@ const Admin = () => {
             <Navbar/>
             <div className="admin-content">
                 <Switch>
-                    <Route path="admin/products">
+                    <Route path="/admin/products">
                         <h1>Product CRUD</h1>
                     </Route>
-                    <Route path="admin/categories">
+                    <Route path="/admin/categories">
                         <h1>Category CRUD</h1>
                     </Route>
-                    <Route path="admin/users">
+                    <Route path="/admin/users">
                         <h1>User CRUD</h1>
                     </Route>
                 </Switch>
