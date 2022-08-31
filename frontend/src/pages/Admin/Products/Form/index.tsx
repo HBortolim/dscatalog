50953ef4f3324084fffdc89152ca99a131e78aa9
Henrique Bortolim <hbortolim@gmail.com>
2022-08-31 20:34:18 -0300

    product routes

---
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -0,0 +1,8 @@
+
+const Form = () => {
+    return(
+        <h1>Form Produtos</h1>
+    );
+}
+
+export default Form;
--- frontend/src/pages/Admin/index.tsx
+++ frontend/src/pages/Admin/index.tsx
@@ -1,28 +1,29 @@
 import PrivateRoute from "components/PrivateRoute";
 import { Switch } from "react-router-dom";
 import Navbar from "./Navbar";
+import Products from "./Products";
 import "./styles.css";
-import Users from "./User";
+import Users from "./Users";
 
 const Admin = () => {
-    return(
-        <div className="admin-container">
-            <Navbar/>
-            <div className="admin-content">
-                <Switch>
-                    <PrivateRoute path="/admin/products" >
-                        <h1>Product CRUD</h1>
-                    </PrivateRoute>
-                    <PrivateRoute path="/admin/categories">
-                        <h1>Category CRUD</h1>
-                    </PrivateRoute>
-                    <PrivateRoute path="/admin/users" roles={["ROLE_ADMIN"]}>
-                        <Users/>
-                    </PrivateRoute>
-                </Switch>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className="admin-container">
+      <Navbar />
+      <div className="admin-content">
+        <Switch>
+          <PrivateRoute path="/admin/products">
+            <Products />
+          </PrivateRoute>
+          <PrivateRoute path="/admin/categories">
+            <h1>Category CRUD</h1>
+          </PrivateRoute>
+          <PrivateRoute path="/admin/users" roles={["ROLE_ADMIN"]}>
+            <Users />
+          </PrivateRoute>
+        </Switch>
+      </div>
+    </div>
+  );
+};
 
 export default Admin;
