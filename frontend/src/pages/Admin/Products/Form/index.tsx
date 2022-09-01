e0d926c1a09bb163aad2a119fd54cf7e9c96b77d
Henrique Bortolim <hbortolim@gmail.com>
2022-09-01 18:31:59 -0300

    ProductCrudForm html

--- frontend/src/pages/Admin/Products/Form/index.tsx
+++ frontend/src/pages/Admin/Products/Form/index.tsx
@@ -1,8 +1,28 @@
+import "./styles.css";
 
 const Form = () => {
-    return(
-        <h1>Form Produtos</h1>
-    );
-}
+  return (
+    <div className="product-crud-container">
+      <div className="base-card product-crud-form-card"></div>
+      <h1 className="product-crud-form-title">Dados do Produto</h1>
+      <form action="">
+        <div className="row">
+            <div className="col-lg-6">
+                <input type="text" name="" id="" className="form-control base-input"/>
+                <input type="text" name="" id="" className="form-control base-input"/>
+                <input type="text" name="" id="" className="form-control base-input"/>
+            </div>
+            <div className="col-lg-6">
+                <textarea name="" id="" cols={30} rows={10} className="form-control base-input"></textarea>
+            </div>
+        </div>
+        <div className="row">
+            <button className="btn btn-outline-danger">Cancelar</button>
+            <button className="btn btn-outline-primary">Salvar</button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Form;
