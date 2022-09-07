9dddb2c8a31a40811b36d88d77fbbf8dd8ac2c83
Henrique Bortolim <hbortolim@gmail.com>
2022-09-06 22:34:42 -0300

    ProductFilter input name layout

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -22,8 +22,16 @@ const Pagination = ({ pageCount, range, onChange }: Props) => {
       activeLinkClassName="pagination-link-active"
       disabledClassName="arrow-inactive"
       onPageChange={(items) => (onChange ? onChange(items.selected) : {})}
-      previousLabel={ <div className="pagination-arrow-container"><ArrowIcon /></div>}
-      nextLabel={<div className="pagination-arrow-container"><ArrowIcon /></div>}
+      previousLabel={
+        <div className="pagination-arrow-container">
+          <ArrowIcon />
+        </div>
+      }
+      nextLabel={
+        <div className="pagination-arrow-container">
+          <ArrowIcon />
+        </div>
+      }
     />
   );
 };
